5a78cea84b6dab33a8e9c8a7fbc38222337451dc
melianmiko <support@mmk.pw>
2023-12-10 20:23:58 +0700

    Fix

--- src/component/MobileOnly.tsx
+++ src/component/MobileOnly.tsx
@@ -2,15 +2,10 @@ import {PropsWithChildren} from "react";
 
 export function MobileOnly(props: PropsWithChildren) {
     return (
-        <div style={{background: "#191c1f"}}>
-            <div style={{
-                maxWidth: 600,
-                margin: "auto",
-                background: "var(--bs-body-bg)",
-                boxShadow: "0px 0px 16px 16px rgba(0,0,0,0.1)"
-            }}>
-                {props.children}
-            </div>
+        <div style={{
+            maxWidth: 600,
+        }}>
+            {props.children}
         </div>
     )
 }
